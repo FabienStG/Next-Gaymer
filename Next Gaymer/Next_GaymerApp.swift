42d200c820fb2a6a8aba28ef9c35a4381a37be37
Fabien Saint Germain <fabienstg@icloud.com>
2022-01-13 10:46:36 +0100

    Initial Commit

---
+++ Next Gaymer/Next_GaymerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Next_GaymerApp.swift
+//  Next Gaymer
+//
+//  Created by Fabien Saint Germain on 13/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Next_GaymerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

2b4a63ca6579d591bd3aa89128258f5f08b2c4bb
FabienStG <fsgermain@corp.free.fr>
2022-11-21 16:23:24 +0100

    Test,gitSSH

--- Next Gaymer/Model/Event Model/FirebaseEventServices.swift
+++ Next Gaymer/Model/Event Model/FirebaseEventServices.swift
@@ -48,6 +48,7 @@ class FirebaseEventServices: EventServices {
         return completionHandler(true, url.absoluteString)
       }
     }
+      print("testGitSSh")
   }
   
   /// Take all the informations from the event form registered by the user and the url from saved image, and save it in Firestore as new object
